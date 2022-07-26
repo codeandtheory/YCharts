448931a9840d2e876724c41a4cd5e05ced3edd76
Deepak KK <deepak.kk@ymedialabs.com>
2022-07-26 12:11:57 +0530

    Created ychart dependency file. Added build src folder.

---
+++ buildSrc/src/main/java/com/ygraph/dependency/YChartDependency.kt
@@ -0,0 +1,32 @@
+package com.ygraph.dependency
+
+object Version {
+    const val CORE_KTX = "1.7.0"
+    const val APPCOMPAT = "1.4.2"
+    const val MATERIAL = "1.6.1"
+    const val JUNIT = "4.13.2"
+    const val TEST_EXTN = "1.1.3"
+    const val ESPRESSO_CORE = "3.4.0"
+    const val COMPOSE_COMPILER = "1.2.0"
+    const val COMPOSE_UI = "1.2.0-rc03"
+    const val COMPOSE_ACTIVITY = "1.3.1"
+    const val RUNTIME_KTX = "2.3.1'"
+}
+
+object YChartDependency {
+    const val CORE_KTX = "androidx.core:core-ktx:${Version.CORE_KTX}"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:${Version.APPCOMPAT}"
+    const val MATERIAL = "com.google.android.material:material:${Version.MATERIAL}"
+    const val JUNIT = "junit:junit:${Version.JUNIT}"
+    const val TEST_EXTN = "androidx.test.ext:junit:${Version.TEST_EXTN}"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Version.ESPRESSO_CORE}"
+    const val COMPOSE_UI = "androidx.compose.ui:ui:${Version.COMPOSE_UI}"
+    const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Version.COMPOSE_UI}"
+    const val COMPOSE_TOOLING_PREVIEW ="androidx.compose.ui:ui-tooling-preview:${Version.COMPOSE_UI}"
+    const val COMPOSE_ACTIVITY = "androidx.activity:activity-compose:${Version.COMPOSE_ACTIVITY}"
+    const val COMPOSE_JUNIT = "androidx.compose.ui:ui-test-junit4:${Version.COMPOSE_UI}"
+    const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:${Version.COMPOSE_UI}"
+    const val COMPOSE_UI_TEST_MANIFEST = "androidx.compose.ui:ui-test-manifest:${Version.COMPOSE_UI}"
+    const val RUNTIME_KTX = "androidx.lifecycle:lifecycle-runtime-ktx:${Version.RUNTIME_KTX}"
+
+}
