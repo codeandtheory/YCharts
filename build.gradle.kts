987a671128dde65d391c5f0b60e1407c4cb84fb4
Preetham Dsouza <preetham.dsouza@ymedialabs.com>
2022-08-03 12:48:12 +0530

    Code refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         classpath("org.jetbrains.kotlin:kotlin-android-extensions:1.7.0")
     }
 
--- buildSrc/src/main/java/com/ygraph/dependency/YChartDependency.kt
+++ buildSrc/src/main/java/com/ygraph/dependency/YChartDependency.kt
@@ -1,18 +1,18 @@
 package com.ygraph.dependency
 
 object YChartDependency {
-    const val CORE_KTX = "androidx.core:core-ktx:${Version.CORE_KTX}"
-    const val APPCOMPAT = "androidx.appcompat:appcompat:${Version.APPCOMPAT}"
-    const val MATERIAL = "com.google.android.material:material:${Version.MATERIAL}"
-    const val JUNIT = "junit:junit:${Version.JUNIT}"
-    const val TEST_EXTN = "androidx.test.ext:junit:${Version.TEST_EXTN}"
-    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Version.ESPRESSO_CORE}"
-    const val COMPOSE_UI = "androidx.compose.ui:ui:${Version.COMPOSE_UI}"
-    const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Version.COMPOSE_UI}"
-    const val COMPOSE_TOOLING_PREVIEW ="androidx.compose.ui:ui-tooling-preview:${Version.COMPOSE_UI}"
-    const val COMPOSE_ACTIVITY = "androidx.activity:activity-compose:${Version.COMPOSE_ACTIVITY}"
-    const val COMPOSE_JUNIT = "androidx.compose.ui:ui-test-junit4:${Version.COMPOSE_UI}"
-    const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:${Version.COMPOSE_UI}"
-    const val COMPOSE_UI_TEST_MANIFEST = "androidx.compose.ui:ui-test-manifest:${Version.COMPOSE_UI}"
-    const val RUNTIME_KTX = "androidx.lifecycle:lifecycle-runtime-ktx:${Version.RUNTIME_KTX}"
+    val CORE_KTX by lazy { "androidx.core:core-ktx:${Version.CORE_KTX}" }
+    val APPCOMPAT by lazy { "androidx.appcompat:appcompat:${Version.APPCOMPAT}" }
+    val MATERIAL by lazy { "com.google.android.material:material:${Version.MATERIAL}" }
+    val JUNIT by lazy { "junit:junit:${Version.JUNIT}" }
+    val TEST_EXTN by lazy { "androidx.test.ext:junit:${Version.TEST_EXTN}" }
+    val ESPRESSO_CORE by lazy { "androidx.test.espresso:espresso-core:${Version.ESPRESSO_CORE}" }
+    val COMPOSE_UI by lazy { "androidx.compose.ui:ui:${Version.COMPOSE_UI}" }
+    val COMPOSE_MATERIAL by lazy { "androidx.compose.material:material:${Version.COMPOSE_UI}" }
+    val COMPOSE_TOOLING_PREVIEW by lazy { "androidx.compose.ui:ui-tooling-preview:${Version.COMPOSE_UI}" }
+    val COMPOSE_ACTIVITY by lazy { "androidx.activity:activity-compose:${Version.COMPOSE_ACTIVITY}" }
+    val COMPOSE_JUNIT by lazy { "androidx.compose.ui:ui-test-junit4:${Version.COMPOSE_UI}" }
+    val COMPOSE_UI_TOOLING by lazy { "androidx.compose.ui:ui-tooling:${Version.COMPOSE_UI}" }
+    val COMPOSE_UI_TEST_MANIFEST by lazy { "androidx.compose.ui:ui-test-manifest:${Version.COMPOSE_UI}" }
+    val RUNTIME_KTX by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Version.RUNTIME_KTX}" }
 }
