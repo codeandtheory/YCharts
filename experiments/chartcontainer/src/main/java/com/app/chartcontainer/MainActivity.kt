56a8843ea00b8e4be08bec420021146972598def
Preetham Dsouza <preetham.dsouza@ymedialabs.com>
2022-08-04 13:12:29 +0530

    Aligned the Yaxis left side

--- experiments/chartcontainer/src/main/java/com/app/chartcontainer/MainActivity.kt
+++ experiments/chartcontainer/src/main/java/com/app/chartcontainer/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
