d6ee928076766460b63fb8c0a28d8ee3cd0f2bed
Preetham Dsouza <preetham.dsouza@ymedialabs.com>
2022-08-04 18:47:01 +0530

    [Feature] Add YAxis data builder to set all params needed to draw YAxis
    - Added a builder class `YAxisData`
    - Modified the YAxis composable to accept only `YAxisData`
    - Code refacotr

--- experiments/chartcontainer/src/main/java/com/app/chartcontainer/MainActivity.kt
+++ experiments/chartcontainer/src/main/java/com/app/chartcontainer/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.unit.sp
 import com.app.chartcontainer.ui.theme.YGraphsTheme
 import com.ygraph.components.axis.Gravity
 import com.ygraph.components.axis.YAxis
+import com.ygraph.components.axis.YAxisData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,33 +30,40 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxWidth(),
                         horizontalArrangement = Arrangement.Start
                     ) {
-                        YAxis(
-                            modifier = Modifier
-                                .height(300.dp),
-                            yMaxValue = 1000f,
-                            yStepValue = 100f,
-                            bottomPadding = 10.dp,
-                            axisLabelFontSize = 14.sp, yLabelData = { index ->
-                                 index.toString() + "k"
-                            }
-                        )
+                        // Right Aligned Yaxis
+                        val leftAlignYAxisData = YAxisData.Builder()
+                            .modifier(
+                                Modifier
+                                    .height(300.dp)
+                            )
+                            .yMaxValue(800f)
+                            .yStepValue(100f)
+                            .bottomPadding(10.dp)
+                            .axisPos(Gravity.LEFT)
+                            .axisLabelFontSize(14.sp)
+                            .yLabelData { index -> index.toString() + "k" }
+                            .build()
+                        YAxis(yAxisData = leftAlignYAxisData)
                     }
+
                     // Right Aligned Yaxis
                     Row(
                         modifier = Modifier.fillMaxWidth(),
                         horizontalArrangement = Arrangement.End
                     ) {
-                        YAxis(
-                            modifier = Modifier
-                                .height(300.dp),
-                            yMaxValue = 1000f,
-                            yStepValue = 100f,
-                            bottomPadding = 10.dp,
-                            axisPos = Gravity.RIGHT,
-                            axisLabelFontSize = 14.sp, yLabelData = { index ->
-                                index.toString() + "k"
-                            }
-                        )
+                        val rightAlignYAxisData = YAxisData.Builder()
+                            .modifier(
+                                Modifier
+                                    .height(300.dp)
+                            )
+                            .yMaxValue(1000f)
+                            .yStepValue(100f)
+                            .bottomPadding(10.dp)
+                            .axisPos(Gravity.RIGHT)
+                            .axisLabelFontSize(14.sp)
+                            .yLabelData { index -> index.toString() + "k" }
+                            .build()
+                        YAxis(yAxisData = rightAlignYAxisData)
                     }
                 }
             }
