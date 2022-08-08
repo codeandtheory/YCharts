1474f05c0b53d61d2318421188cddbdc91926127
Preetham Ivan Dsouza <107846675+preetham1316@users.noreply.github.com>
2022-08-08 10:40:13 +0530

    [Feature] Add YAxis data builder to set all params needed to draw YAxis (#6)
    - Added a builder class `YAxisData`
    - Modified the YAxis composable to accept only `YAxisData`
    - Added axis config data model
    - Added axis line color from yaxis data
    - Added param link as per javadoc
    
    Co-authored-by: Preetham Dsouza <preetham.dsouza@ymedialabs.com>

--- YGraphlib/src/main/java/com/ygraph/components/axis/YAxis.kt
+++ YGraphlib/src/main/java/com/ygraph/components/axis/YAxis.kt
@@ -15,8 +15,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ygraph.components.common.extensions.getTextHeight
@@ -25,108 +23,88 @@ import com.ygraph.components.common.extensions.getTextWidth
 /**
  *
  * YAxis compose method using for drawing yAxis in bar graph.
- * @param modifier : All modifier related property
- * @param yMaxValue: yAxis max value
- * @param yStepValue: Step value for label segmentation
- * @param bottomPadding: X,Y Label offset bottom padding
- * @param yLabelData(Int)->String: lambda method for providing labels,
- * @param axisLabelFontSize: Font size of axis lablel data
- * @param axisPos : Axis gravity
- * @param textLabelPadding: Text label padding from y Axis
- * @param yAxisOffset: Drawing offset for yAxis.
- * @param lineStrokeWidth: Thickness of yAxis line
- * @param topPadding: X,Y Label offset top padding
- * @param indicatorLineWidth: Indicator width on Y axis line for showing points
+ * @param yAxisData : All data needed to draw Yaxis
+ * @see com.ygraph.components.axis.YAxisData Data class to save all params related to Yaxis
  */
 @Composable
-fun YAxis(
-    modifier: Modifier,
-    yMaxValue: Float,
-    yStepValue: Float,
-    bottomPadding: Dp,
-    yLabelData: (Int) -> String,
-    yAxisLineColor: Color = Color.Black,
-    axisLabelFontSize: TextUnit = 14.sp,
-    axisPos: Gravity = Gravity.LEFT,
-    textLabelPadding: Dp = 4.dp,
-    yAxisOffset: Dp = 30.dp,
-    lineStrokeWidth: Dp = 2.dp,
-    topPadding: Dp = 20.dp,
-    indicatorLineWidth: Dp = 5.dp
-) {
-    var yAxisWidth by remember { mutableStateOf(0.dp) }
-    val isRightAligned = axisPos == Gravity.RIGHT
-    Column(modifier = modifier.clipToBounds()) {
-        Canvas(
-            modifier = modifier
-                .clipToBounds()
-                .width(yAxisWidth)
-                .background(Color.White)
-        ) {
-            val yAxisHeight = size.height - bottomPadding.roundToPx()
-            var yMax = yMaxValue
-            var reqYLabelsQuo = (yMaxValue / yStepValue) + 1 // Added one since it starts from 0
-            val reqYLabelsRem = yMaxValue.rem(yStepValue)
-            if (reqYLabelsRem > 0f) {
-                reqYLabelsQuo += 2
-                yMax = (yMaxValue - reqYLabelsRem) + yStepValue * 2
-            }
-            // Minus the top padding to avoid cropping at the top
-            val segmentHeight = (yAxisHeight - topPadding.toPx()) / yMax
+fun YAxis(yAxisData: YAxisData) {
+    with(yAxisData) {
+        var yAxisWidth by remember { mutableStateOf(0.dp) }
+        val isRightAligned = axisPos == Gravity.RIGHT
+        Column(modifier = modifier.clipToBounds()) {
+            Canvas(
+                modifier = modifier
+                    .clipToBounds()
+                    .width(yAxisWidth)
+                    .background(Color.White)
+            ) {
+                val yAxisHeight = size.height - bottomPadding.roundToPx()
+                var yMax = yMaxValue
+                var reqYLabelsQuo = (yMaxValue / yStepValue) + 1 // Added one since it starts from 0
+                val reqYLabelsRem = yMaxValue.rem(yStepValue)
+                if (reqYLabelsRem > 0f) {
+                    reqYLabelsQuo += 2
+                    yMax = (yMaxValue - reqYLabelsRem) + yStepValue * 2
+                }
+                // Minus the top padding to avoid cropping at the top
+                val segmentHeight = (yAxisHeight - topPadding.toPx()) / yMax
 
-            val yAxisTextPaint = TextPaint().apply {
-                textSize = axisLabelFontSize.toPx()
-                color = Color.Black.toArgb()
-                textAlign = if (isRightAligned) Paint.Align.RIGHT else Paint.Align.LEFT
-            }
-            for (i in 0 until reqYLabelsQuo.toInt()) {
-                //Drawing the axis labels
-                drawContext.canvas.nativeCanvas.apply {
-                    if (i != reqYLabelsQuo.toInt()) {
-                        val yAxisLabel = yLabelData(i)
-                        val width = yAxisLabel.getTextWidth(yAxisTextPaint)
-                        val height: Int = yAxisLabel.getTextHeight(yAxisTextPaint)
-                        if (width > yAxisWidth.toPx()) {
-                            yAxisWidth = width.toDp() + yAxisOffset
+                val yAxisTextPaint = TextPaint().apply {
+                    textSize = axisLabelFontSize.toPx()
+                    color = yAxisLineColor.toArgb()
+                    textAlign = if (isRightAligned) Paint.Align.RIGHT else Paint.Align.LEFT
+                }
+                for (i in 0 until reqYLabelsQuo.toInt()) {
+                    //Drawing the axis labels
+                    drawContext.canvas.nativeCanvas.apply {
+                        if (i != reqYLabelsQuo.toInt()) {
+                            val yAxisLabel = yLabelData(i)
+                            val width = yAxisLabel.getTextWidth(yAxisTextPaint)
+                            val height: Int = yAxisLabel.getTextHeight(yAxisTextPaint)
+                            if (width > yAxisWidth.toPx()) {
+                                yAxisWidth = width.toDp() + yAxisOffset
+                            }
+                            drawText(
+                                yAxisLabel,
+                                if (isRightAligned) yAxisWidth.toPx() - textLabelPadding.toPx() else textLabelPadding.toPx(),
+                                yAxisHeight + height / 2 - ((segmentHeight * (i * yStepValue))),
+                                yAxisTextPaint
+                            )
                         }
-                        drawText(
-                            yAxisLabel,
-                            if (isRightAligned) yAxisWidth.toPx() - textLabelPadding.toPx() else textLabelPadding.toPx(),
-                            yAxisHeight + height / 2 - ((segmentHeight * (i * yStepValue))),
-                            yAxisTextPaint
+                    }
+                    if (axisConfig.isAxisLineRequired) {
+                        // Draw line only until reqYLabelsQuo -1 else will be a extra line at top with no label
+                        if (i != (reqYLabelsQuo.toInt() - 1)) {
+                            //Draw Yaxis line
+                            drawLine(
+                                start = Offset(
+                                    x = if (isRightAligned) 0.dp.toPx() else yAxisWidth.toPx(),
+                                    y = yAxisHeight - (segmentHeight * (i * yStepValue))
+                                ),
+                                end = Offset(
+                                    x = if (isRightAligned) 0.dp.toPx() else yAxisWidth.toPx(),
+                                    y = yAxisHeight - (segmentHeight * ((i + 1) * yStepValue))
+                                ),
+                                color = yAxisLineColor, strokeWidth = lineStrokeWidth.toPx()
+                            )
+                        }
+
+                        //Draw pointer lines on Yaxis
+                        drawLine(
+                            start = Offset(
+                                x = if (isRightAligned) 0.dp.toPx() else {
+                                    yAxisWidth.toPx() - indicatorLineWidth.toPx()
+                                },
+                                y = yAxisHeight - (segmentHeight * (i * yStepValue))
+                            ),
+                            end = Offset(
+                                x = if (isRightAligned) indicatorLineWidth.toPx() else yAxisWidth.toPx(),
+                                y = yAxisHeight - (segmentHeight * (i * yStepValue))
+                            ),
+                            color = yAxisLineColor, strokeWidth = lineStrokeWidth.toPx()
                         )
                     }
                 }
-                // Draw line only until reqYLabelsQuo -1 else will be a extra line at top with no label
-                if (i != (reqYLabelsQuo.toInt() - 1)) {
-                    //Draw Yaxis line
-                    drawLine(
-                        start = Offset(
-                            x = if (isRightAligned) 0.dp.toPx() else yAxisWidth.toPx(),
-                            y = yAxisHeight - (segmentHeight * (i * yStepValue))
-                        ),
-                        end = Offset(
-                            x = if (isRightAligned) 0.dp.toPx() else yAxisWidth.toPx(),
-                            y = yAxisHeight - (segmentHeight * ((i + 1) * yStepValue))
-                        ),
-                        color = yAxisLineColor, strokeWidth = lineStrokeWidth.toPx()
-                    )
-                }
-
-                //Draw pointer lines on Yaxis
-                drawLine(
-                    start = Offset(
-                        x = if (isRightAligned) 0.dp.toPx() else {
-                            yAxisWidth.toPx() - indicatorLineWidth.toPx()
-                        },
-                        y = yAxisHeight - (segmentHeight * (i * yStepValue))
-                    ),
-                    end = Offset(
-                        x = if (isRightAligned) indicatorLineWidth.toPx() else yAxisWidth.toPx(),
-                        y = yAxisHeight - (segmentHeight * (i * yStepValue))
-                    ),
-                    color = yAxisLineColor, strokeWidth = lineStrokeWidth.toPx()
-                )
             }
         }
     }
@@ -135,15 +113,15 @@ fun YAxis(
 @Preview(showBackground = true)
 @Composable
 fun YAxisPreview() {
-    YAxis(
-        modifier = Modifier
-            .height(300.dp),
-        yMaxValue = 400f,
-        yStepValue = 100f,
-        bottomPadding = 10.dp,
-        axisLabelFontSize = 14.sp, yLabelData = { index ->
-            index.toString()
-        }
-    )
+    val yAxisData = YAxisData.Builder()
+        .modifier(Modifier.height(300.dp))
+        .yMaxValue(800f)
+        .yStepValue(100f)
+        .bottomPadding(10.dp)
+        .axisPos(Gravity.LEFT)
+        .axisLabelFontSize(14.sp)
+        .yLabelData { index -> index.toString() }
+        .build()
+    YAxis(yAxisData = yAxisData)
 }
 
---
+++ YGraphlib/src/main/java/com/ygraph/components/axis/YAxisData.kt
@@ -0,0 +1,111 @@
+package com.ygraph.components.axis
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ *
+ * YAxis data class params used in drawing yAxis in any graph.
+ * @param modifier : All modifier related property
+ * @param yMaxValue: yAxis max value
+ * @param yStepValue: Step value for label segmentation
+ * @param bottomPadding: X,Y Label offset bottom padding
+ * @param yLabelData(Int)->String: lambda method for providing labels,
+ * @param axisLabelFontSize: Font size of axis lablel data
+ * @param axisPos : Axis gravity
+ * @param textLabelPadding: Text label padding from y Axis
+ * @param yAxisOffset: Drawing offset for yAxis.
+ * @param lineStrokeWidth: Thickness of yAxis line
+ * @param topPadding: X,Y Label offset top padding
+ * @param indicatorLineWidth: Indicator width on Y axis line for showing points
+ * @param axisConfig: All config related param to toggle the elements while drawing graph
+ */
+data class YAxisData(
+    val modifier: Modifier,
+    val yMaxValue: Float,
+    val yStepValue: Float,
+    val yLabelData: (Int) -> String,
+    val yAxisLineColor: Color,
+    val axisLabelFontSize: TextUnit,
+    val axisPos: Gravity,
+    val textLabelPadding: Dp,
+    val yAxisOffset: Dp,
+    val lineStrokeWidth: Dp,
+    val topPadding: Dp,
+    val bottomPadding: Dp,
+    val indicatorLineWidth: Dp,
+    val axisConfig: AxisConfig
+) {
+    class Builder {
+        private var modifier: Modifier = Modifier
+        private var yMaxValue: Float = 0f
+        private var yStepValue: Float = 0f
+        private var yLabelData: (Int) -> String = { _ -> "" }
+        private var yAxisLineColor: Color = Color.Black
+        private var axisLabelFontSize: TextUnit = 14.sp
+        private var axisPos: Gravity = Gravity.LEFT
+        private var textLabelPadding: Dp = 4.dp
+        private var yAxisOffset: Dp = 30.dp
+        private var lineStrokeWidth: Dp = 2.dp
+        private var topPadding: Dp = 20.dp
+        private var bottomPadding: Dp = 10.dp
+        private var indicatorLineWidth: Dp = 5.dp
+        private var axisConfig = AxisConfig(isAxisLineRequired = true)
+
+        fun modifier(modifier: Modifier) = apply { this.modifier = modifier }
+
+        fun yMaxValue(maxValue: Float) = apply { this.yMaxValue = maxValue }
+
+        fun yStepValue(stepValue: Float) = apply { this.yStepValue = stepValue }
+
+        fun bottomPadding(padding: Dp) = apply { this.bottomPadding = padding }
+
+        fun yLabelData(labelData: (Int) -> String) = apply { this.yLabelData = labelData }
+
+        fun yAxisLineColor(lineColor: Color) = apply { this.yAxisLineColor = lineColor }
+
+        fun axisLabelFontSize(fontSize: TextUnit) = apply { this.axisLabelFontSize = fontSize }
+
+        fun axisPos(pos: Gravity) = apply { this.axisPos = pos }
+
+        fun textLabelPadding(padding: Dp) = apply { this.textLabelPadding = padding }
+
+        fun yAxisOffset(offset: Dp) = apply { this.yAxisOffset = offset }
+
+        fun lineStrokeWidth(strokeWidth: Dp) = apply { this.lineStrokeWidth = strokeWidth }
+
+        fun topPadding(padding: Dp) = apply { this.topPadding = padding }
+
+        fun indicatorLineWidth(lineWidth: Dp) = apply { this.indicatorLineWidth = lineWidth }
+
+        fun axisConfig(config: AxisConfig) = apply { this.axisConfig = config }
+
+        fun build() = YAxisData(
+            modifier,
+            yMaxValue,
+            yStepValue,
+            yLabelData,
+            yAxisLineColor,
+            axisLabelFontSize,
+            axisPos,
+            textLabelPadding,
+            yAxisOffset,
+            lineStrokeWidth,
+            topPadding,
+            bottomPadding,
+            indicatorLineWidth,
+            axisConfig
+        )
+    }
+}
+
+/**
+ *
+ * AxisConfig data class used to mention all config related param required to draw graph.
+ * @param isAxisLineRequired : true if should show the axis and points on the line else false
+ */
+data class AxisConfig(val isAxisLineRequired: Boolean)
