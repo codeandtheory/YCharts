3c016cce9f80b6197fec80be930a2228f359d35e
Preetham Ivan Dsouza <107846675+preetham1316@users.noreply.github.com>
2022-08-09 16:16:43 +0530

    [Feature] Drawing YAxis as per given gravity(Left/Right) (#5)
    - Aligned the Yaxis left side
    - Handled Y axis to be aligned both left and right side
    - Removed redundant clases and code
    - Added a builder class `YAxisData`
    - Modified the YAxis composable to accept only `YAxisData`
    - Added axis config data model
    - Added axis line color from yaxis data
    - Added param link as per javadoc
    -Added extensions, removed unused class and updated javadoc
    
    Co-authored-by: Preetham Dsouza <preetham.dsouza@ymedialabs.com>

---
+++ YGraphlib/src/main/java/com/ygraph/components/axis/YAxis.kt
@@ -0,0 +1,183 @@
+package com.ygraph.components.axis
+
+import android.graphics.Paint
+import android.text.TextPaint
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ygraph.components.common.extensions.getTextHeight
+import com.ygraph.components.common.extensions.getTextWidth
+
+/**
+ *
+ * YAxis compose method using for drawing yAxis in bar graph.
+ * @param yAxisData : All data needed to draw Yaxis
+ * @see com.ygraph.components.axis.YAxisData Data class to save all params related to Yaxis
+ */
+@Composable
+fun YAxis(yAxisData: YAxisData) {
+    with(yAxisData) {
+        var yAxisWidth by remember { mutableStateOf(0.dp) }
+        val isRightAligned = axisPos == Gravity.RIGHT
+        Column(modifier = modifier.clipToBounds()) {
+            Canvas(
+                modifier = modifier
+                    .clipToBounds()
+                    .width(yAxisWidth)
+                    .background(backgroundColor)
+            ) {
+                val (yAxisHeight, reqYLabelsQuo, segmentHeight) = getAxisInitValues(yAxisData)
+                val yAxisTextPaint = TextPaint().apply {
+                    textSize = axisLabelFontSize.toPx()
+                    color = yAxisLineColor.toArgb()
+                    textAlign = if (isRightAligned) Paint.Align.RIGHT else Paint.Align.LEFT
+                }
+                for (i in 0 until reqYLabelsQuo.toInt()) {
+                    // Drawing the axis labels
+                    yAxisWidth = drawAxisLabel(
+                        i,
+                        reqYLabelsQuo,
+                        yAxisData,
+                        yAxisTextPaint,
+                        yAxisWidth,
+                        isRightAligned,
+                        yAxisHeight,
+                        segmentHeight
+                    )
+                    drawAxisLineWithPointers(
+                        yAxisData,
+                        i,
+                        reqYLabelsQuo,
+                        isRightAligned,
+                        yAxisWidth,
+                        yAxisHeight,
+                        segmentHeight
+                    )
+                }
+            }
+        }
+    }
+}
+
+private fun DrawScope.getAxisInitValues(
+    yAxisData: YAxisData
+): Triple<Float, Float, Float> {
+    val yAxisHeight = size.height - yAxisData.bottomPadding.roundToPx()
+    var yMax = yAxisData.yMaxValue
+    var reqYLabelsQuo =
+        (yAxisData.yMaxValue / yAxisData.yStepValue) + 1 // Added one since it starts from 0
+    val reqYLabelsRem = yAxisData.yMaxValue.rem(yAxisData.yStepValue)
+    if (reqYLabelsRem > 0f) {
+        reqYLabelsQuo += 1
+        yMax = (yAxisData.yMaxValue - reqYLabelsRem) + yAxisData.yStepValue
+    }
+    // Minus the top padding to avoid cropping at the top
+    val segmentHeight = (yAxisHeight - yAxisData.topPadding.toPx()) / yMax
+    return Triple(yAxisHeight, reqYLabelsQuo, segmentHeight)
+}
+
+
+private fun DrawScope.drawAxisLineWithPointers(
+    yAxisData: YAxisData,
+    i: Int,
+    reqYLabelsQuo: Float,
+    isRightAligned: Boolean,
+    yAxisWidth: Dp,
+    yAxisHeight: Float,
+    segmentHeight: Float
+) {
+    if (yAxisData.axisConfig.isAxisLineRequired) {
+        // Draw line only until reqYLabelsQuo -1 else will be a extra line at top with no label
+        if (i != (reqYLabelsQuo.toInt() - 1)) {
+            //Draw Yaxis line
+            drawLine(
+                start = Offset(
+                    x = if (isRightAligned) 0.dp.toPx() else yAxisWidth.toPx(),
+                    y = yAxisHeight - (segmentHeight * (i * yAxisData.yStepValue))
+                ),
+                end = Offset(
+                    x = if (isRightAligned) 0.dp.toPx() else yAxisWidth.toPx(),
+                    y = yAxisHeight - (segmentHeight * ((i + 1) * yAxisData.yStepValue))
+                ),
+                color = yAxisData.yAxisLineColor, strokeWidth = yAxisData.lineStrokeWidth.toPx()
+            )
+        }
+
+        //Draw pointer lines on Yaxis
+        drawLine(
+            start = Offset(
+                x = if (isRightAligned) 0.dp.toPx() else {
+                    yAxisWidth.toPx() - yAxisData.indicatorLineWidth.toPx()
+                },
+                y = yAxisHeight - (segmentHeight * (i * yAxisData.yStepValue))
+            ),
+            end = Offset(
+                x = if (isRightAligned) yAxisData.indicatorLineWidth.toPx() else yAxisWidth.toPx(),
+                y = yAxisHeight - (segmentHeight * (i * yAxisData.yStepValue))
+            ),
+            color = yAxisData.yAxisLineColor, strokeWidth = yAxisData.lineStrokeWidth.toPx()
+        )
+    }
+}
+
+
+private fun DrawScope.drawAxisLabel(
+    index: Int,
+    reqYLabelsQuo: Float,
+    yAxisData: YAxisData,
+    yAxisTextPaint: TextPaint,
+    yAxisWidth: Dp,
+    isRightAligned: Boolean,
+    yAxisHeight: Float,
+    segmentHeight: Float
+): Dp {
+    var calculatedYAxisWidth = yAxisWidth
+    drawContext.canvas.nativeCanvas.apply {
+        if (index != reqYLabelsQuo.toInt()) {
+            val yAxisLabel = yAxisData.yLabelData(index)
+            val width = yAxisLabel.getTextWidth(yAxisTextPaint)
+            val height: Int = yAxisLabel.getTextHeight(yAxisTextPaint)
+            if (width > calculatedYAxisWidth.toPx()) {
+                calculatedYAxisWidth = width.toDp() + yAxisData.yAxisOffset
+            }
+            drawText(
+                yAxisLabel,
+                if (isRightAligned) calculatedYAxisWidth.toPx() - yAxisData.textLabelPadding.toPx() else {
+                    yAxisData.textLabelPadding.toPx()
+                },
+                yAxisHeight + height / 2 - ((segmentHeight * (index * yAxisData.yStepValue))),
+                yAxisTextPaint
+            )
+        }
+    }
+    return calculatedYAxisWidth
+}
+
+@Preview(showBackground = true)
+@Composable
+fun YAxisPreview() {
+    val yAxisData = YAxisData.Builder()
+        .modifier(Modifier.height(300.dp))
+        .yMaxValue(800f)
+        .yStepValue(100f)
+        .bottomPadding(10.dp)
+        .axisPos(Gravity.LEFT)
+        .axisLabelFontSize(14.sp)
+        .yLabelData { index -> index.toString() }
+        .build()
+    YAxis(yAxisData = yAxisData)
+}
+
--- experiments/chartcontainer/src/main/java/com/app/chartcontainer/MainActivity.kt
+++ experiments/chartcontainer/src/main/java/com/app/chartcontainer/MainActivity.kt
@@ -3,20 +3,16 @@ package com.app.chartcontainer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.app.chartcontainer.ui.theme.YGraphsTheme
-import com.ygraph.components.bar.YAxis
+import com.ygraph.components.axis.Gravity
+import com.ygraph.components.axis.YAxis
+import com.ygraph.components.axis.YAxisData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,36 +20,46 @@ class MainActivity : ComponentActivity() {
         setContent {
             YGraphsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Yellow)
                 ) {
-                    YAxis(
-                        modifier = Modifier
-                            .height(200.dp)
-                            .width(20.dp).padding(top = 50.dp, bottom = 50.dp, start = 50.dp),
-                        yMaxValue = 600f,
-                        yStepValue = 100f,
-                        labelOffSet = 10.dp,
-                        axisLabelFontSize = 14.sp, yLabelData = { index ->
-                            return@YAxis index.toString()
-                        }
-                    )
+                    // Left Aligned Yaxis
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start
+                    ) {
+                        val leftAlignYAxisData = YAxisData.Builder()
+                            .modifier(Modifier.height(300.dp))
+                            .yMaxValue(800f)
+                            .yStepValue(90f)
+                            .bottomPadding(10.dp)
+                            .axisPos(Gravity.LEFT)
+                            .axisLabelFontSize(14.sp)
+                            .yLabelData { index -> ((90f).toInt() * index).toString() }
+                            .build()
+                        YAxis(yAxisData = leftAlignYAxisData)
+                    }
+
+                    // Right Aligned Yaxis
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        val rightAlignYAxisData = YAxisData.Builder()
+                            .modifier(Modifier.height(300.dp))
+                            .yMaxValue(1000f)
+                            .yStepValue(100f)
+                            .bottomPadding(10.dp)
+                            .axisPos(Gravity.RIGHT)
+                            .axisLabelFontSize(14.sp)
+                            .yLabelData { index -> ((100f).toInt() * index).toString() }
+                            .build()
+                        YAxis(yAxisData = rightAlignYAxisData)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    YGraphsTheme {
-        Greeting("Android")
     }
 }
