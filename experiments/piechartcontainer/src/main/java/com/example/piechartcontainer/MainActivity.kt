a22bbf1f0f54d6d65eca36b9e6838ea5e5399a1f
Margin KS <margin.ks@ymedialabs.com>
2022-08-16 15:20:56 +0530

    no message

--- experiments/piechartcontainer/src/main/java/com/example/piechartcontainer/MainActivity.kt
+++ experiments/piechartcontainer/src/main/java/com/example/piechartcontainer/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -37,9 +38,12 @@ class MainActivity : ComponentActivity() {
                     
                     val pieChartData = PieChartData(
                         slices = listOf(
-                            PieChartData.Slice("Very looooong label", 15f, Color(0xFF58BDFF)),
-                            PieChartData.Slice("Android", 35f, Color(0xFF125B7F)),
-                            PieChartData.Slice("iOS", 40f, Color(0xFF092D40)),
+                            PieChartData.Slice("MI", 15f, Color(0xFF58BDFF)),
+                            PieChartData.Slice("Iphone", 35f, Color(0xFF125B7F)),
+                            PieChartData.Slice("Windows", 10f, Color(0xFF092D40)),
+                            PieChartData.Slice("Pixel", 10f, Color(0xFF092D10)),
+                            PieChartData.Slice("Samsung", 20f, Color(0xFF092D05)),
+                            PieChartData.Slice("Oneplus", 0f, Color(0xFF092D20)),
                         )
                     )
                     
@@ -54,7 +58,8 @@ class MainActivity : ComponentActivity() {
                             isEllipsizeEnabled = true,
                             sliceLabelEllipsizeAt = TextUtils.TruncateAt.MIDDLE,
                             percentageTypeface = Typeface.defaultFromStyle(Typeface.ITALIC),
-                            isAnimationEnable = true
+                            isAnimationEnable = true,
+                            showSliceLabels = true
                         )
                     
                     PieChart(modifier = Modifier.padding(10.dp), pieChartData, pieChartConfig) { slice ->
