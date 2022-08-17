903cb3b1dc29ea58260ffaf1bd5d48207778a34e
Margin KS <margin.ks@ymedialabs.com>
2022-08-17 19:31:47 +0530

    Added Barchart experiment module Data classes for bar chart Dummy barchart Composable

---
+++ experiments/barchartcontainer/src/main/java/com/example/barchartcontainer/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.barchartcontainer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.barchartcontainer.ui.theme.YGraphsTheme
+import com.ygraph.components.axis.YAxisData
+import com.ygraph.components.barchart.BarChart
+import com.ygraph.components.barchart.models.BarChartData
+import com.ygraph.components.barchart.models.BarData
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            YGraphsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                    val barData = getBarChartData(50,50)
+                    val yAxisData = YAxisData.Builder()
+                        .modifier(Modifier.height(300.dp))
+                        .yMaxValue(50f)
+                        .yStepValue(5f)
+                        .build()
+
+                    val barChartData = BarChartData(
+                        chartData = barData, yAxisData = yAxisData
+                    )
+
+                    BarChart(modifier = Modifier.height(300.dp), barChartData =barChartData)
+                }
+            }
+        }
+    }
+}
+
+private fun getBarChartData(listSize: Int, maxRange: Int): List<BarData> {
+    val list = arrayListOf<BarData>()
+    for (index in 0 until listSize) {
+        list.add(
+            BarData(
+                index.toFloat(),
+                Random.nextDouble(1.0, maxRange.toDouble()).toFloat(),
+                Color(
+                    Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)
+                ),
+                index.toString()
+            )
+        )
+    }
+    return list
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    YGraphsTheme {
+        Greeting("Android")
+    }
+}
