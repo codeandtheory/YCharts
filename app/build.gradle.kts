dcb70316382c2c8c4e897d16034199634ed52b62
Preetham Dsouza <preetham.dsouza@ymedialabs.com>
2022-08-29 09:39:16 +0530

    Refactored gradle lib dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":YGraphlib"))
+    implementation(project(mapOf("path" to ":YGraphlib")))
     implementation(YChartDependency.CORE_KTX)
     implementation(YChartDependency.COMPOSE_UI)
     implementation(YChartDependency.COMPOSE_MATERIAL)
