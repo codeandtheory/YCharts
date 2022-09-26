c6dc8f05c953cdabb3dba619e070f72d27432176
Preetham Dsouza <preetham.dsouza@ymedialabs.com>
2022-09-26 11:02:27 +0530

    Data class code reactor

--- YGraphlib/src/main/java/com/ygraph/components/graph/combinedgraph/model/CombinedGraphData.kt
+++ YGraphlib/src/main/java/com/ygraph/components/graph/combinedgraph/model/CombinedGraphData.kt
@@ -17,13 +17,9 @@ import com.ygraph.components.graph.linegraph.model.Line
  * @param yAxisData: All the configurations related to Y-Axis to be defined here in [AxisData]
  * @param barPlotData: Data related to the drawing of bar graph.
  * @param paddingTop: Padding from the top of the canvas to start of the graph container.
- * @param paddingRight: Padding from the end of the canvas to end of the graph container.
  * @param bottomPadding: Padding from the bottom of the canvas to bottom of the graph container.
  * @param containerPaddingEnd: Container inside padding end after the last point of the graph.
  * @param backgroundColor: Background color of the Y & X components.
- * @param selectionHighLightPopUp: Adds customization related to selection highlight of bar and point with popup.
- * @param canSupportTapOnIndividualPointOrBar: True if user wants tap on individual graph component say
- * point on a line or bar else false where tap will be considered for all the point and bar in the tap range
  */
 data class CombinedGraphData(
     val linePlotData: List<Line>,
@@ -36,8 +32,7 @@ data class CombinedGraphData(
     val horizontalExtraSpace: Dp = 10.dp,
     val containerPaddingEnd: Dp = 15.dp,
     val backgroundColor: Color = Color.White,
-    val tapPadding: Dp = 10.dp,
-    val canSupportTapOnIndividualPointOrBar: Boolean = true
+    val tapPadding: Dp = 10.dp
 )
 
 /**
