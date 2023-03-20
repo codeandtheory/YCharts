98669a923a884d1d43e68324ffbeb035b02af6b7
Harsha Kiran <harsha.kiran@ymedialabs.com>
2023-03-20 11:35:16 +0530

    - Removed the commented lines.

--- androidApp/src/main/java/co/yml/ycharts/app/presentation/LineChartActivity.kt
+++ androidApp/src/main/java/co/yml/ycharts/app/presentation/LineChartActivity.kt
@@ -60,32 +60,10 @@ class LineChartActivity : ComponentActivity() {
                             .padding(it),
                         contentAlignment = Alignment.TopCenter
                     ) {
-//                        LazyColumn(content = {
-//                            items(3) { item ->
-//                                when (item) {
-//                                    0 -> LineGraph1(
-//                                        DataUtils.getLineChartData(
-//                                            100,
-//                                            start = 50,
-//                                            maxRange = 100
-//                                        )
-//                                    )
-//                                    1 -> LineGraph2(DataUtils.getLineChartData(50, maxRange = 200))
-//                                    2 -> LineGraph3(
-//                                        DataUtils.getLineChartData(
-//                                            200,
-//                                            start = -50,
-//                                            maxRange = 50
-//                                        )
-//                                    )
-//                                }
-//                            }
-//                        })
                         Box(modifier = Modifier.padding(it)) {
                             CommonMainScreen(2)
                         }
                     }
-//
                 }
             }
         }
