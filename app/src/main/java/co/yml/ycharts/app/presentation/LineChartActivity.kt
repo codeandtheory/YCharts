46c013da611b96022d414e7a8725b07dc1e323f6
AkYML <anantha.krishna@ymedialabs.com>
2023-04-10 16:37:20 +0530

    Update LineChartActivity.kt
    update samples

--- app/src/main/java/co/yml/ycharts/app/presentation/LineChartActivity.kt
+++ app/src/main/java/co/yml/ycharts/app/presentation/LineChartActivity.kt
@@ -145,6 +145,23 @@ class LineChartActivity : ComponentActivity() {
                                     .fillMaxWidth()
                                     .height(1.dp))
                             }
+                            item {
+                                Text(
+                                    modifier=Modifier.padding(12.dp),
+                                    text = getString(R.string.combined_line_chart),
+                                    style = MaterialTheme.typography.subtitle1,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                CombinedLinechartWithBackground(DataUtils.getLineChartData(
+                                            200,
+                                            start = -50,
+                                            maxRange = 50
+                                        ))
+                                Spacer(modifier = Modifier.height(12.dp))
+                                Divider(modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(1.dp))
+                            }
                         })
                     }
                 }
@@ -467,6 +484,119 @@ private fun CombinedLinechart(pointsData: List<Point>) {
         xAxisData = xAxisData,
         yAxisData = yAxisData,
         gridLines = GridLines()
+    )
+
+    Column(
+        modifier = Modifier
+            .height(500.dp)
+    ) {
+        LineChart(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp),
+            lineChartData = data
+        )
+        Legends(
+            legendsConfig = legendsConfig
+        )
+    }
+}
+@Composable
+private fun CombinedLinechartWithBackground(pointsData: List<Point>) {
+    val steps = 5
+    val xAxisData = AxisData.Builder()
+        .axisStepSize(30.dp)
+        .steps(pointsData.size - 1)
+        .backgroundColor(Color.Yellow)
+        .labelData { i -> i.toString() }
+        .labelAndAxisLinePadding(15.dp)
+        .build()
+    val yAxisData = AxisData.Builder()
+        .steps(steps)
+        .backgroundColor(Color.Yellow)
+        .labelAndAxisLinePadding(20.dp)
+        .labelData { i ->
+            // Add yMin to get the negative axis values to the scale
+            val yMin = pointsData.minOf { it.y }
+            val yMax = pointsData.maxOf { it.y }
+            val yScale = (yMax - yMin) / steps
+            ((i * yScale) + yMin).formatToSinglePrecision()
+        }.build()
+    val colorPaletteList = listOf<Color>(Color.Blue,Color.Yellow,Color.Magenta,Color.DarkGray)
+    val legendsConfig = LegendsConfig(
+        legendLabelList = DataUtils.getLegendsLabelData(colorPaletteList),
+        gridColumnCount = 4
+    )
+    val data = LineChartData(
+        linePlotData = LinePlotData(
+            lines = listOf(
+                Line(
+                    dataPoints = pointsData,
+                    lineStyle = LineStyle(
+                        lineType = LineType.SmoothCurve(isDotted = true),
+                        color = colorPaletteList.first()
+                    ),
+                    shadowUnderLine = ShadowUnderLine(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color.Green,
+                                Color.Transparent
+                            )
+                        ), alpha = 0.3f
+                    ),
+                    selectionHighlightPoint = SelectionHighlightPoint(
+                        color = Color.Green
+                    ),
+                    selectionHighlightPopUp = SelectionHighlightPopUp(
+                        backgroundColor = Color.Black,
+                        backgroundStyle = Stroke(2f),
+                        labelColor = Color.Red,
+                        labelTypeface = Typeface.DEFAULT_BOLD
+                    )
+                ),
+                Line(
+                    dataPoints = pointsData.subList(10,20),
+                    lineStyle = LineStyle(lineType = LineType.SmoothCurve(), color = colorPaletteList[1]),
+                    intersectionPoint = IntersectionPoint(color = Color.Red),
+                    selectionHighlightPopUp = SelectionHighlightPopUp(popUpLabel = { x, y ->
+                        val xLabel = "x : ${(1900 + x).toInt()} "
+                        val yLabel = "y : ${String.format("%.2f", y)}"
+                        "$xLabel $yLabel"
+                    })
+                ),
+                Line(
+                    dataPoints = DataUtils.getLineChartData(
+                        20,
+                        start = 0,
+                        maxRange = 50
+                    ),
+                    LineStyle(color = colorPaletteList[2]),
+                    IntersectionPoint(),
+                    SelectionHighlightPoint(),
+                    shadowUnderLine = ShadowUnderLine(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color.Cyan,
+                                Color.Blue
+                            )
+                        ), alpha = 0.5f
+                    ),
+                    SelectionHighlightPopUp()
+                ),
+                Line(
+                    dataPoints = pointsData.subList(10, 20),
+                    LineStyle(color = colorPaletteList[3]),
+                    IntersectionPoint(),
+                    SelectionHighlightPoint(),
+                    ShadowUnderLine(),
+                    SelectionHighlightPopUp()
+                )
+            )
+        ),
+        xAxisData = xAxisData,
+        yAxisData = yAxisData,
+        gridLines = GridLines(),
+        backgroundColor = Color.Yellow
     )
 
     Column(
